ecdcc2b82410d9118193509de6c9bd791b2c001c
t.onodera <t.onodera.1.2.1.5@gmail.com>
2025-01-19 22:38:11 +0900

    バックエンド側の実装完了

--- scripts/local-deploy.ts
+++ scripts/local-deploy.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+import { MyERC721__factory } from "../typechain-types";
 
 async function main() {
   const myToken = await ethers.deployContract("MyToken");
@@ -9,6 +10,14 @@ async function main() {
   const myERC20 = await ethers.deployContract("MyERC20");
   await myERC20.waitForDeployment();
   console.log(`MyERC20 deployed to: ${myERC20.target}`);
+
+  // MyERC721をデプロイ
+  const myERC721 = await ethers.deployContract("MyERC721", [
+    "MyERC721",
+    "MYERC721",
+  ]);
+  await myERC721.waitForDeployment();
+  console.log(`MyERC20 deployed to: ${myERC721.target}`);
 }
 
 main().catch(console.error);
